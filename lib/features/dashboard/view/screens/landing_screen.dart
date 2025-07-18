82d0985b7b0feb10ac1e05bdb21b279af2edbcc5
Sabinkc <sabinkc1206@gmail.com>
2025-07-18 13:32:45 +0545

    fixed bottom navigation

--- lib/features/dashboard/view/screens/landing_screen.dart
+++ lib/features/dashboard/view/screens/landing_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:kitchen_ecommerce/common/colors.dart';
 import 'package:kitchen_ecommerce/features/dashboard/view/screens/home_screen.dart';
 import 'package:kitchen_ecommerce/features/cart/view/screens/cart_screen.dart';
@@ -28,109 +29,86 @@ class _LandingScreenState extends ConsumerState<LandingScreen> {
     return Scaffold(
       extendBody: true,
       body: screens[lanRef.selIndex],
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.all(15),
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(50),
-            color: ComColors.priColor,
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: lanRef.selIndex,
+        onTap: (index) => lanRef.updateInd(index),
+        backgroundColor: ComColors.priColor,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white.withValues(alpha: 0.6),
+        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
+        items: [
+          BottomNavigationBarItem(
+            icon: Container(
+              padding: EdgeInsets.all(8.r),
+              decoration: BoxDecoration(
+                color: lanRef.selIndex == 0 ? Colors.white : Colors.transparent,
+                borderRadius: BorderRadius.circular(50),
+              ),
+              child: Icon(
+                lanRef.selIndex == 0 ? Icons.home : Icons.home_outlined,
+                color: lanRef.selIndex == 0
+                    ? ComColors.priLightColor
+                    : Colors.white,
+                size: 30.r,
+              ),
+            ),
+            label: 'Home',
           ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              // 0: Home
-              InkWell(
-                onTap: () => lanRef.updateInd(0),
-                child: Container(
-                  padding: const EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    color: lanRef.selIndex == 0
-                        ? Colors.white
-                        : ComColors.priColor,
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                  child: Icon(
-                    lanRef.selIndex == 0 ? Icons.home : Icons.home_outlined,
-                    color: lanRef.selIndex == 0
-                        ? ComColors.priLightColor
-                        : Colors.white,
-                    size: 30,
-                  ),
-                ),
+          BottomNavigationBarItem(
+            icon: Container(
+              padding: EdgeInsets.all(8.r),
+              decoration: BoxDecoration(
+                color: lanRef.selIndex == 1 ? Colors.white : Colors.transparent,
+                borderRadius: BorderRadius.circular(50),
               ),
-
-              // 1: Bag
-              InkWell(
-                onTap: () => lanRef.updateInd(1),
-                child: Container(
-                  padding: const EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    color: lanRef.selIndex == 1
-                        ? Colors.white
-                        : ComColors.priColor,
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                  child: Icon(
-                    lanRef.selIndex == 1
-                        ? Icons.shopping_cart
-                        : Icons.shopping_cart_outlined,
-                    color: lanRef.selIndex == 1
-                        ? ComColors.priLightColor
-                        : Colors.white,
-                    size: 30,
-                  ),
-                ),
+              child: Icon(
+                lanRef.selIndex == 1
+                    ? Icons.shopping_cart
+                    : Icons.shopping_cart_outlined,
+                color: lanRef.selIndex == 1
+                    ? ComColors.priLightColor
+                    : Colors.white,
+                size: 30.r,
               ),
-
-              // 2: Favorite
-              InkWell(
-                onTap: () => lanRef.updateInd(2),
-                child: Container(
-                  padding: const EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    color: lanRef.selIndex == 2
-                        ? Colors.white
-                        : ComColors.priColor,
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                  child: Icon(
-                    lanRef.selIndex == 2
-                        ? Icons.favorite
-                        : Icons.favorite_outline,
-                    color: lanRef.selIndex == 2
-                        ? ComColors.priLightColor
-                        : Colors.white,
-                    size: 30,
-                  ),
-                ),
-              ),
-
-              // 3: Settings
-              InkWell(
-                onTap: () => lanRef.updateInd(3),
-                child: Container(
-                  padding: const EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    color: lanRef.selIndex == 3
-                        ? Colors.white
-                        : ComColors.priColor,
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                  child: Icon(
-                    lanRef.selIndex == 3
-                        ? Icons.person_2
-                        : Icons.person_2_outlined,
-                    color: lanRef.selIndex == 3
-                        ? ComColors.priLightColor
-                        : Colors.white,
-                    size: 30,
-                  ),
-                ),
-              ),
-            ],
+            ),
+            label: 'Cart',
           ),
-        ),
+          BottomNavigationBarItem(
+            icon: Container(
+              padding: EdgeInsets.all(8.r),
+              decoration: BoxDecoration(
+                color: lanRef.selIndex == 2 ? Colors.white : Colors.transparent,
+                borderRadius: BorderRadius.circular(50),
+              ),
+              child: Icon(
+                lanRef.selIndex == 2 ? Icons.favorite : Icons.favorite_outline,
+                color: lanRef.selIndex == 2
+                    ? ComColors.priLightColor
+                    : Colors.white,
+                size: 30.r,
+              ),
+            ),
+            label: 'Wishlist',
+          ),
+          BottomNavigationBarItem(
+            icon: Container(
+              padding: EdgeInsets.all(8.r),
+              decoration: BoxDecoration(
+                color: lanRef.selIndex == 3 ? Colors.white : Colors.transparent,
+                borderRadius: BorderRadius.circular(50),
+              ),
+              child: Icon(
+                lanRef.selIndex == 3 ? Icons.person_2 : Icons.person_2_outlined,
+                color: lanRef.selIndex == 3
+                    ? ComColors.priLightColor
+                    : Colors.white,
+                size: 30.r,
+              ),
+            ),
+            label: 'Profile',
+          ),
+        ],
       ),
     );
   }
